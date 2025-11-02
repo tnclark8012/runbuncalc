2879b75ee8d5b5f02980a653cfe905f3949ed187
Taylor Clark <1501159+tnclark8012@users.noreply.github.com>
2025-11-02 17:37:25 +0000

    weather and terrain

--- src/extensions/simulator/phases/turn-start/start-of-turn-abilities.ts
+++ src/extensions/simulator/phases/turn-start/start-of-turn-abilities.ts
@@ -16,6 +16,7 @@ export function applyStartOfTurnAbilities(state: BattleFieldState): BattleFieldS
   for (let participant of participants) {
     applyAbilityToSelf(participant.source);
     participant.opponents?.forEach(opponent => applyAbilityToTarget(participant.source, opponent));
+    applyAbilityToField(participant.source, state);
   }
 
   return state;
@@ -41,5 +42,47 @@ function applyAbilityToTarget(source: ActivePokemon, target: ActivePokemon): voi
     if (target.pokemon.hasAbility('Rattled')) {
       applyBoost(target.pokemon.boosts, 'spe', 1);
     }
+  }
+}
+
+function applyAbilityToField(source: ActivePokemon, state: BattleFieldState): void {
+  if (!source.firstTurnOut)
+    return;
+
+  if (source.pokemon.hasAbility('Drought')) {
+    state.cpuField.weather = "Sun";
+    state.playerField.weather = "Sun";
+  }
+  else if (source.pokemon.hasAbility('Drizzle')) {
+    state.cpuField.weather = "Rain";
+    state.playerField.weather = "Rain";
+  }
+  else if (source.pokemon.hasAbility('Sand Stream')) {
+    state.cpuField.weather = "Sand";
+    state.playerField.weather = "Sand";
+  }
+  else if (source.pokemon.hasAbility('Snow Warning')) {
+    state.cpuField.weather = "Hail";
+    state.playerField.weather = "Hail";
+  }
+  else if (source.pokemon.hasAbility('Cloud Nine', 'Air Lock')) {
+    state.cpuField.weather = undefined;
+    state.playerField.weather = undefined;
+  }
+  else if (source.pokemon.hasAbility('Electric Surge')) {
+    state.cpuField.terrain = "Electric";
+    state.playerField.terrain = "Electric";
+  }
+  else if (source.pokemon.hasAbility('Grassy Surge')) {
+    state.cpuField.terrain = "Grassy";
+    state.playerField.terrain = "Grassy";
+  }
+  else if (source.pokemon.hasAbility('Misty Surge')) {
+    state.cpuField.terrain = "Misty";
+    state.playerField.terrain = "Misty";
+  }
+  else if (source.pokemon.hasAbility('Psychic Surge')) {
+    state.cpuField.terrain = "Psychic";
+    state.playerField.terrain = "Psychic";
   }
 }
